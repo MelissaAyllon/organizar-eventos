e5bde3e70f138af9f0c11107b02768f8b024003f
MelissaAyllon <mayllon@espol.edu.ec>
2025-08-23 17:04:10 -0500

    feat: add new migration for events schema

---
+++ database/migrations/2025_08_23_213524_add_new_columns_to_your_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->string('tipo_actividad')->nullable();
+            $table->string('organizador')->nullable();
+            $table->integer('capacidad_maxima')->nullable();
+            $table->string('estado')->default('activo');
+            $table->integer('comments_count')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            //
+        });
+    }
+};
